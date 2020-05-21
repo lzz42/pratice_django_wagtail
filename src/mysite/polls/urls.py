eda837efd3f24568f09f42c4a3ca8d9e2224d56b
Zima <seainchina@163.com>
2020-05-21 13:01:05 +0800

    新增视图内容练习

--- src/mysite/polls/urls.py
+++ src/mysite/polls/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 
 from . import views
 
-urlpatterns =[
-    path('',views.index,name='index'),
-    path(route='admin',view= views.admin,kwargs=None,name='admin')
-    path("<int:question_id>/", views.detail, name="detail")
-    path("<int:question_id>/results/", views.results, name="results")
-    path("<int:question_id>/vote/", views.vote, name="vote")
+app_name ='polls'
+urlpatterns = [
+    path(route='', view=views.index, kwargs=None, name='index'),
+    path(route='admin', view=views.admin, kwargs=None, name='admin'),
+    # path(route='<int:question_id>/', view=views.detail, kwargs=None, name="detail"),
+    # path(route='<int:question_id>/results/',
+    #      view=views.results, kwargs=None, name="results"),
+    path(route='<int:question_id>/', view=views.detail, kwargs=None, name="detail"),
+    path(route='<int:question_id>/results/',
+         view=views.results, kwargs=None, name="results"),
+    path(route='<int:question_id>/vote/',
+         view=views.vote, kwargs=None, name="vote"),
 ]
--- src/mysite/polls/views.py
+++ src/mysite/polls/views.py
@@ -1,19 +1,52 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_list_or_404,get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question,Choice
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello you're at polls index")
+    # 获取数据
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # 载入模板
+    template = loader.get_template('polls/index.html')
+    # 构建上下文
+    context = {
+        'latest_question_list':latest_question_list,
+    }
+    # 生成响应
+    # return HttpResponse(template.render(context,request))
+    return render(request,'polls/index.html',context)
+    # output = ','.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    # return HttpResponse("Hello you're at polls index")
 
 def admin(request):
     return HttpResponse("helle this is Admin")
 
 def detail(request,question_id):
-    return HttpResponse("You are looking question: %s"%question_id)
+    try:
+        q = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question ID %s not exist"%str(question_id))
+    return render(request,'polls/detail.html',{'question':q})
+    # return HttpResponse("You are looking question: %s"%question_id)
 
-def results(requst,question_id):
-    rep = HttpResponse("You are looking question: %s"%question_id)
-    return HttpResponse(rep%question_id)
+def results(request,question_id):
+    q = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':q})
 
-def vote(resquest,question_id):
-    return  HttpResponse("You are vating on question: %s"%question_id)
+def vote(request,question_id):
+    q = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = q.choice_set.get(pk=request.POST['choice'])
+        pass
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{'question':q,'error_message':"You do not select a choice"})
+        pass
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+    return  HttpResponseRedirect(reverse('polls:results',args=(q.id,)))
